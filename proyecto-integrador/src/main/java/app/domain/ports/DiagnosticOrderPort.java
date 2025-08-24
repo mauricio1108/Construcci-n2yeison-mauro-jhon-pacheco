bc65b69fec88cd794a29837b6955738b479a072b
DiYei10 <ceferibe@gmail.com>
2025-08-23 22:53:16 -0500

    Se agregan mas servicios y se corrigen errores. /JhonOquendo_YeisonUribe_MauricioJaramillo_JuanPacheco

--- proyecto-integrador/src/main/java/app/domain/ports/DiagnosticOrderPort.java
+++ proyecto-integrador/src/main/java/app/domain/ports/DiagnosticOrderPort.java
@@ -1,11 +1,14 @@
 package app.domain.ports;
 
+import app.domain.model.DiagnosticAidOrder;
 import java.util.List;
 import app.domain.model.Patient;
 
-
 public interface DiagnosticOrderPort {
+
     public DiagnosticOrderPort findById(DiagnosticOrderPort diagnosticorder) throws Exception;
-	public List<DiagnosticOrderPort> findByPatient(Patient patient)throws Exception;
-	public void save(DiagnosticOrderPort diagnosticOrder) throws Exception;
+
+    public List<DiagnosticAidOrder> findByPatient(Patient patient) throws Exception;
+
+    public void save(DiagnosticOrderPort diagnosticOrder) throws Exception;
 }
--- proyecto-integrador/src/main/java/app/domain/services/SearchDiagnosticAidOrderByPatient.java
+++ proyecto-integrador/src/main/java/app/domain/services/SearchDiagnosticAidOrderByPatient.java
@@ -1,5 +1,20 @@
 package app.domain.services;
 
+import app.domain.ports.DiagnosticOrderPort;
+import app.domain.model.DiagnosticAidOrder;
+import app.domain.model.Patient;
+import app.domain.ports.PatientPort;
+import java.util.List;
+
 public class SearchDiagnosticAidOrderByPatient {
+    private PatientPort patientPort;
+    private DiagnosticOrderPort diagnosticAidOrder;
     
+    public List<DiagnosticAidOrder> search(Patient patient) throws Exception{
+        patient = patientPort.findById(patient);
+        if (patient == null){
+            throw new Exception("Debe contar con un paciente registrado");
+        }
+        return diagnosticAidOrder.findByPatient(patient);
+    }
 }
